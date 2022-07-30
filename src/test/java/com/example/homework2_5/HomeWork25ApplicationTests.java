f7072c523f0bbde6a8cf040506ad4dbbb3ac5002
Ilya <primerasem@gmail.com>
2022-07-31 00:29:59 +0900

    Initial commit

---
+++ src/test/java/com/example/homework2_5/HomeWork25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework2_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
