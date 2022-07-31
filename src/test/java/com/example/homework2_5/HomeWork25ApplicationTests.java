466fa55a77405f989627db7dd4999b1351f9b3a5
Ilya <primerasem@gmail.com>
2022-07-31 17:15:40 +0900

    Задание по листам

--- src/test/java/com/example/homework2_5/HomeWork25ApplicationTests.java
+++ src/test/java/com/example/homework2_5/HomeWork25ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HomeWork25ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
